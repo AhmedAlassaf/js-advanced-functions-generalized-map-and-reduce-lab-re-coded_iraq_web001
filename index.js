9e99b7ebce2b6ab23e5ffbe7e034dd8e3380d815
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-07-15 09:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function map(src, callBack){
 
 function reduce(src, callBack, SP){
   let nVal = SP ? SP : src[0];
-  for (let i= SP ? 0 : 1; i<src.length; i++)
+  for (let i= SP ? 0 : 1; i<src.length; i++){
+    
+  }
   
 }
