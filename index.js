a7d86ade42ee9447a7eb0ccf219c13c3105fbefe
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-07-15 09:28:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+function map(src, callBack){
+  let nArr = []
+  src.forEach(val => nArr.push(callBack(val)))
+  return nArr
+}
+
+function reduce(src, callBack, SP){
+  
+}
